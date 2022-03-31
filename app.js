073fa022ae7b7cc77832c78b50ed8599817453c4
olacase@gmail.com <olacase@gmail.com>
2022-03-31 06:30:39 -0500

    isterted html

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
+const fs = require('fs');
 const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-// this
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
 
-  console.log('================');
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if(err) throw new Error (err);
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+  console.log('portfolio complete! check out index.html to see the output!')
+});
 
-printProfileData(profileDataArgs);
+
+
+
 
 
